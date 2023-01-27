b58f8ec187ccfde0c2f15e4204d3b5018cdbb1b9
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-01-27 17:54:44 +0000

    make controlled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = React.useState();
+
+  function handleSelectionChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
-    <select multiple={false}>
+    // <select multiple={false}>
+    <select multiple={false} value={value} onChange={handleSelectionChange}>
       <option value='1'>A</option>
       <option value='2'>B</option>
       <option value='3'>C</option>
