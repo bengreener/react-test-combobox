a74fba8c05f6a54938485a48b9248d6fdf57cfe6
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-01-27 17:20:20 +0000

    add test renders combobox

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,22 @@
-import { logRoles, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders select listbox', async () => {
+test('renders combobox', async () => {
+  const component = render(<App />);
+
+  expect(screen.getByRole('option', {name: 'A'}).selected).toBe(true);
+  expect(screen.getByRole('option', {name: 'B'}).selected).toBe(false);
+  expect(screen.getByRole('option', {name: 'C'}).selected).toBe(false);
+});
+
+it('changes selection in combobox', async () => {
   const user = UserEvent.setup();
   const component = render(<App />);
-  // logRoles(component.container);
 
-  // await user.selectOptions(screen.getByRole('listbox'), ['1', 'C'])
-  await user.selectOptions(screen.getByRole('combobox'), '1')
+  await user.selectOptions(screen.getByRole('combobox'), 'B')
 
-  expect(screen.getByRole('option', {name: 'A'}).selected).toBe(true)
-  expect(screen.getByRole('option', {name: 'B'}).selected).toBe(false)
-  // expect(screen.getByRole('option', {name: 'C'}).selected).toBe(true)
-  expect(screen.getByRole('option', {name: 'C'}).selected).toBe(false)
-});
+  expect(screen.getByRole('option', {name: 'A'}).selected).toBe(false);
+  expect(screen.getByRole('option', {name: 'B'}).selected).toBe(true);
+  expect(screen.getByRole('option', {name: 'C'}).selected).toBe(false);
+})
